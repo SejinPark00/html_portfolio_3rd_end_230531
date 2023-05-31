0f714c015b7a9af420e71b37a333e0d86886200f
SejinPark00 <126125603+SejinPark00@users.noreply.github.com>
2023-05-31 17:18:26 +0900

    Delete js directory

--- js/Categori.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    $('.categori > li').mouseenter(function(){
-        $('.CtgMenu').stop().hide()
-        $(this).find('.CtgMenu').stop().fadeIn();
-    }).mouseleave(function(){
-        $('.CtgMenu').stop().hide()
-    });
-});
--- js/js/SubMenu.js
+++
@@ -1,9 +0,0 @@
-/* section #2 메뉴이동 */
-
-$(function () {
-	$(".SubMenu ul li").click(function () {
-		var i = $(this).index();
-		$(".SubMenu ul li").removeClass("on1");
-		$(".SubMenu ul li").eq(i).addClass("on1");
-	})
-});
